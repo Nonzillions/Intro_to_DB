9fc41edd61ccce43d714a1995228e2ce1c0e78bd
Nonzillions <obiechefuchinonso@gmail.com>
2025-10-21 11:12:47 +0100

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -16,7 +16,7 @@ CREATE TABLE Books (
 
 CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(100),
+    customer_name VARCHAR(100) NOT NULL,
     email VARCHAR(100)
 );
 
